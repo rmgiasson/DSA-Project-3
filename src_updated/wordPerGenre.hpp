b6a456bf02dd0aeb117c6eb3667d8f8239941b32
Tanushree Hadavale <147886719+thadavale@users.noreply.github.com>
2024-08-06 19:06:15 -0400

    Update wordPerGenre.hpp

--- src_updated/wordPerGenre.hpp
+++ src_updated/wordPerGenre.hpp
@@ -3,8 +3,11 @@
 #include <string>
 #include <vector>
 #include <chrono>
+#include "unorderedMapStructure.hpp"
+#include "mapStructure.hpp"
 
-using namespace std::chrono;
+using namespace std;
+using namespace chrono;
 
 void wordPerGenreUnordered(UnorderedMapStructure umap, string word) {
     auto start = high_resolution_clock::now();
@@ -12,16 +15,20 @@ void wordPerGenreUnordered(UnorderedMapStructure umap, string word) {
     //look at each genre
     vector<string> v = umap.getGenres();
     //in each genre find the word
-    cout << "Looking for instances of '" << word << "' in the Unordered database." << endl;
+    cout << endl;
+    cout << "Looking for instances of '" << word << "' in the Unordered Map database." << endl;
+    cout << endl;
     for(auto i : v) {
-        //print stuff and things
+        //print word count
         cout << i << ": " << word << " appears " << umap.getWordCount(i, word) << " times." << endl;
     }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
 
+    cout << endl;
     cout << "This function took " << duration.count() << " milliseconds." << endl;
+    cout << endl;
 }
 
 void wordPerGenreOrdered(MapStructure omap, string word) {
@@ -30,14 +37,17 @@ void wordPerGenreOrdered(MapStructure omap, string word) {
     //look at each genre
     vector<string> v = omap.getGenres();
     //in each genre find the word
-    cout << "Looking for instances of '" << word << "' in the Standard database." << endl;
+    cout << "Looking for instances of '" << word << "' in the Standard Map database." << endl;
+    cout << endl;
     for(auto i : v) {
-        //print stuff and things
+        //print word count
         cout << i << ": " << word << " appears " << omap.getWordCount(i, word) << " times." << endl;
     }
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
 
+    cout << endl;
     cout << "This function took " << duration.count() << " milliseconds." << endl;
+    cout << endl;
 }
